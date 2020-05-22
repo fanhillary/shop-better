fdea7ef693099f1bda197584d8ffbcc18febc6e0
Hillary Fan <hillary.fan123@gmail.com>
2020-05-21 18:58:09 -0700

    Added comments and changed name of script

--- scrape_product.py
+++ scrape_product.py
@@ -4,6 +4,11 @@ import requests
 import json
 
 def build_css_selector(important, ignore):
+    """ build css selector string for beautifulsoup to parse pertinent elements
+    important - array of words to look for elements with a class that includes the word in important
+    ignore - array of words to ignore elements with this word in it's class
+    Returns: selector string for searching elements that include important class and don't have ignore class
+    """
     selector = ""
     for word in important:
         selector += "[class*='" + word + "']"
@@ -13,13 +18,23 @@ def build_css_selector(important, ignore):
   
     return selector[:-2]
 
-def check_parent_valid(parent_class, ignore):
+
+def check_ignore_in_string(string, ignore):
+    """ check if the string provided contains a word in the array ignore
+    string - string to check 
+    ignore - array of words to check string for
+    Returns: True - string does not include any words in ignore.
+             False - string includes at least one word in ignore.
+    """
     for word in ignore:
-        if ignore in parent_class:
+        if ignore in string:
             return False
     return True
 
+
 def scrape_page(url):
+    """ scrape url given and returns title of product, price, previous price, styles, photoURL
+    """
     ignore = ["additional", "related", "similar", "cart"]
     important = ["price", "colors"]
     selector = build_css_selector(important, ignore)
@@ -34,7 +49,7 @@ def scrape_page(url):
         for parent in price.parents:
             if 'class' in parent:
                 # if parent class has something from ignore array, break out of looping parents
-                if not check_parent_valid(parent['class'], ignore):
+                if not check_ignore_in_string(parent['class'], ignore):
                     break
         else:
             price_narrow.append(price)
